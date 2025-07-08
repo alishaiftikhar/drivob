18fb5b65117f37a44791beddee1b3e63d183feca
alishaiftikhar <alishaiftikhar025@gmail.com>
2025-07-08 19:20:54 +0500

    create database and add tables in database

---
+++ drivodb.session.sql
@@ -0,0 +1,14 @@
+INSERT INTO admin (id, username, email, password)
+VALUES (
+    id:intINSERT INTO auth_group (id, name)
+    VALUES (id:intINSERT INTO admin (id, username, email, password)
+    VALUES (
+        id:int,
+        'username:varchar',
+        'email:varchar',
+        'password:varchar'
+      );, 'name:varchar');,
+    'username:varchar',
+    'email:varchar',
+    'password:varchar'
+  );
